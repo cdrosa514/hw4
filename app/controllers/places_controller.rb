cbc3f16839f569151eda5be86a82e6ed206c08f7
Christian De Rosa <christian.derosa@kellogg.northwestern.edu>
2024-03-08 23:53:34 +0000

    two

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,12 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.all
-  end
+
+    respond_to do |format|
+      format.html #{ render :template => "places/index" }
+      format.json { render :json => @places }
+    end
+
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
@@ -10,13 +15,32 @@ class PlacesController < ApplicationController
   end
 
   def new
+    @user = User.find_by({ "id" => session["user_id"] })
   end
 
   def create
+  @user = User.find_by({ "id" => session["user_id"] })
+  if @user != nil
     @place = Place.new
     @place["name"] = params["name"]
+    @place["image"] = params["image"]
+    # @place.uploaded_image.attach(params["uploaded_image"])
+    @place["user_id"] = @user["id"]
     @place.save
     redirect_to "/places"
+  else
+    flash["notice"] = "Login first."
   end
+  redirect_to "/places"
+end
 
+  # before_action :allow_cors
+  # def allow_cors
+  #   response.headers['Access-Control-Allow-Origin'] = '*'
+  #   response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+  #   response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+  #   response.headers['Access-Control-Max-Age'] = '1728000'
+  # end
+  
+end
 end
